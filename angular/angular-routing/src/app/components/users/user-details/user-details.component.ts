7c89807a0e2c09df0a353c8f6860051a460d76d2
Johan Smarius <jsmarius@hotmail.com>
2018-11-11 13:46:38 +0100

    Begin status for class

--- angular/angular-routing/src/app/components/users/user-details/user-details.component.ts
+++ angular/angular-routing/src/app/components/users/user-details/user-details.component.ts
@@ -10,18 +10,8 @@ export class UserDetailsComponent implements OnInit {
 
   title = 'User Detail';
   id = -1;
-  
-  constructor(
-    private route: ActivatedRoute
-  ) { }
 
   ngOnInit() {
-    // this.id = +this.route.snapshot.paramMap.get('id');
-
-    this.route.params.subscribe((params) => {
-      this.id = + params['id'];
-    });
-
   }
 
 }
