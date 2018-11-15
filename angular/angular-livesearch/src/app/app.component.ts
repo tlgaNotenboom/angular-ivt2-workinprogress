cd11cee1c18a3b5a763833fc11d79db1fc38a42e
Robin Schellius <r.schellius@avans.nl>
2018-11-16 00:02:43 +0100

    Updated

--- angular/angular-livesearch/src/app/app.component.ts
+++ angular/angular-livesearch/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
-import { Subject } from "rxjs";
-import { map, switchMap, filter, debounceTime, distinct } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -11,30 +8,9 @@ import { map, switchMap, filter, debounceTime, distinct } from "rxjs/operators";
 export class AppComponent {
 
   title = 'Instant search';
-  searchTerm: string;
-  results;
-  githubUrl = `https://api.github.com/search/repositories?q=${this.searchTerm}&sort=stars&order=desc`;
 
-  latestSearch = new Subject<string>();
+  // `https://api.github.com/search/repositories?q=${this.searchTerm}&sort=stars&order=desc`;
 
-  constructor(
-    public http: Http
-  ){
-    this.results = this.latestSearch.pipe(
-      // wait 500 msec before processing the next change
-      debounceTime(500),
-      // only search if the term has not changed
-      distinct(),
-      // do not search for empty terms
-      // so the term must be 'truthy' (true) 
-      filter(term => !!term),
-      // every time the term changes
-      switchMap(term => this.http.get(`https://api.github.com/search/repositories?q=${this.searchTerm}&sort=stars&order=desc`)),
-      map(response => response.json().items.map(item => item.name))
-    )
-  }
+  constructor(){}
       
-  newSearch(term: string) {
-    this.latestSearch.next(term);
-  }
 }
